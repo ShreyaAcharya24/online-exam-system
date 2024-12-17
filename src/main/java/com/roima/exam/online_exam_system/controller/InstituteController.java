ead72c9303210333c7d8b781828d63ff67e47fdf
shreyaacharya24 <shreyaacharya24301@gmail.com>
2024-12-18 01:32:20 +0530

    Added DTO

--- src/main/java/com/roima/exam/online_exam_system/controller/InstituteController.java
+++ src/main/java/com/roima/exam/online_exam_system/controller/InstituteController.java
@@ -1,5 +1,7 @@
 package com.roima.exam.online_exam_system.controller;
 
+import com.roima.exam.online_exam_system.dto.InstituteRequestDTO;
+import com.roima.exam.online_exam_system.dto.InstituteResponseDTO;
 import com.roima.exam.online_exam_system.model.Institute;
 import com.roima.exam.online_exam_system.service.InstituteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/institutes")
 public class InstituteController {
@@ -17,14 +22,34 @@ public class InstituteController {
     @Autowired
     private InstituteService instituteService;
 
-    @PostMapping("/add")
-    public ResponseEntity<Institute> addInEntity(@RequestBody Institute institute){
-        System.out.println("***Inside Post ****");
-        Institute createdInstance = instituteService.addInstitute(institute);
-        System.out.println("Response JSON: " + createdInstance);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdInstance);
+    public InstituteController(InstituteService instituteService) {
+        this.instituteService = instituteService;
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<InstituteResponseDTO> addInstitute(@RequestBody InstituteRequestDTO instituteRequestDTO){
+        System.out.println("***Inside Post ****");
 
+        Institute institute = new Institute();
+        institute.setInstitute_name(instituteRequestDTO.getInstitute_name());
+        institute.setAddress(instituteRequestDTO.getAddress());
+        institute.setContact(instituteRequestDTO.getContact());
 
+        Institute createdInstitute = instituteService.addInstitute(institute);
+
+        InstituteResponseDTO instituteResponseDTO = new InstituteResponseDTO();
+        instituteResponseDTO.setInstitute_name(createdInstitute.getInstitute_name());
+        instituteResponseDTO.setInstiute_id(createdInstitute.getInstitute_id());
+
+//        Map<String, Object> response = new HashMap<>();
+//        response.put("status", "success");
+//        response.put("code", HttpStatus.CREATED.value());
+//        response.put("message", "Institute created successfully");
+//        response.put("data", instituteResponseDTO);
+//        response.put("timestamp", java.time.Instant.now().toString());
+
+        // Return the response wrapped in ResponseEntity
+        return new ResponseEntity<>(instituteResponseDTO, HttpStatus.CREATED);
+
+       }
 }
---
+++ src/main/java/com/roima/exam/online_exam_system/dto/InstituteRequestDTO.java
@@ -0,0 +1,32 @@
+package com.roima.exam.online_exam_system.dto;
+
+public class InstituteRequestDTO {
+
+    private String institute_name;
+    private String contact;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getInstitute_name() {
+        return institute_name;
+    }
+
+    public void setInstitute_name(String institute_name) {
+        this.institute_name = institute_name;
+    }
+}

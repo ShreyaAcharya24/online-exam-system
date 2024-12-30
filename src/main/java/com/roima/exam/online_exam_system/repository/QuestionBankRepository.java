d6a7af36ab47ba514871c721b20337e5f7572110
shreyaacharya24 <shreyaacharya24301@gmail.com>
2024-12-30 19:57:58 +0530

    Untrack Question Related Repository files

--- src/main/java/com/roima/exam/online_exam_system/repository/QuestionBankRepository.java
+++
@@ -1,7 +0,0 @@
-package com.roima.exam.online_exam_system.repository;
-
-import com.roima.exam.online_exam_system.model.QuestionBank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionBankRepository extends JpaRepository<QuestionBank,Integer> {
-}
--- src/main/java/com/roima/exam/online_exam_system/repository/TypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.roima.exam.online_exam_system.repository;
-
-import com.roima.exam.online_exam_system.model.Type;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeRepository extends JpaRepository<Type,Integer> {
-}

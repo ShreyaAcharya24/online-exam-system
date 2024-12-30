37cbf416f9eed5f35769e263e2ab2b02201c4b68
shreyaacharya24 <shreyaacharya24301@gmail.com>
2024-12-30 20:34:12 +0530

    empty main branch

--- src/main/java/com/roima/exam/online_exam_system/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.roima.exam.online_exam_system.repository;
-
-import com.roima.exam.online_exam_system.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}

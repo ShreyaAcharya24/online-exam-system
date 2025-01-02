ff381495115dbb9c77bcf1fb9cb72180550ff8e7
shreyaacharya24 <shreyaacharya24301@gmail.com>
2025-01-02 18:21:30 +0530

    Unwanted files removed

--- src/main/java/com/roima/exam/online_exam_system/repository/ProgramRepository.java
+++
@@ -1,9 +0,0 @@
-package com.roima.exam.online_exam_system.repository;
-
-import com.roima.exam.online_exam_system.model.ProgramAns;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProgramRepository extends JpaRepository<ProgramAns,Integer> {
-}

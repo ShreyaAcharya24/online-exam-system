06e1a2049b8d24b6d73b759ffaeff7247c53120a
shreyaacharya24 <shreyaacharya24301@gmail.com>
2025-01-02 18:13:00 +0530

    Removed unwanted files

--- src/main/java/com/roima/exam/online_exam_system/dto/InstituteDTO.java
+++
@@ -1,6 +0,0 @@
-package com.roima.exam.online_exam_system.dto;
-
-public record InstituteDTO(int instituteID,String institutename,
-                           String contact,
-                           String address) {
-}
